8fb9bed2335f3a6d32dda065182f8fe2c5390468
Talat09 <talat181008312@gmail.com>
2024-10-28 23:17:01 +0600

    set up basic route and controller

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from "express";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
 const app = express();
 const port = process.env.PORT || 5000;
-app.use("/api/V1/test", (req, res) => {
-  res.send("Hello From Real Estate!");
-});
+app.use("/api/V1/posts", postRoute);
+app.use("/api/V1/auth", authRoute);
 app.listen(port, () => {
   console.log("Server listening on Port: ", port);
 });
