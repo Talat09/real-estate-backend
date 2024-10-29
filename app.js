78eaf0e21092e0542a3e759a047c9586b1b21235
Talat09 <talat181008312@gmail.com>
2024-10-29 13:03:10 +0600

    done creating user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(express.json());
 app.use("/api/V1/posts", postRoute);
 app.use("/api/V1/auth", authRoute);
 app.listen(port, () => {
