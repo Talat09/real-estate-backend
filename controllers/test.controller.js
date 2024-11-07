32a2c638f4213a38ff651b6bb08460d267b9a678
Talat09 <talat181008312@gmail.com>
2024-11-08 00:20:01 +0600

    add middleware for verifyToken

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,15 +1,19 @@
 import jwt from "jsonwebtoken";
-export const shouldBeLoggedIn = async (req, res, next) => {
+export const shouldBeLoggedIn = async (req, res) => {
+  console.log(req.userId);
+  res.status(200).json({ message: "You are Authenticated!" });
+};
+export const shouldBeAdmin = async (req, res) => {
   const token = req.cookies.token;
   if (!token)
-    return res
-      .status(401)
-      .json({ message: "Access denied! User Not Authenticated." });
+    return res.status(401).json({ message: "You Are Not Authenticated!" });
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
     if (err) {
       return res.status(403).json({ message: "Token is not valid!" });
     }
+    if (!payload.isAdmin) {
+      return res.status(403).json({ message: "Not Authorized!" });
+    }
     res.status(200).json({ message: "You are Authenticated!" });
   });
 };
-export const shouldBeAdmin = async (req, res, next) => {};
--- routes/test.route.js
+++ routes/test.route.js
@@ -3,10 +3,11 @@ import {
   shouldBeAdmin,
   shouldBeLoggedIn,
 } from "../controllers/test.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Hi from test Routes!");
 });
-router.get("/should-be-logged-in", shouldBeLoggedIn);
+router.get("/should-be-logged-in", verifyToken, shouldBeLoggedIn);
 router.get("/should-be-admin", shouldBeAdmin);
 export default router;
