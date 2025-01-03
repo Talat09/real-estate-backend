f6908c53a30e75d9474038ace90e2ffa157d4483
Talat09 <talat181008312@gmail.com>
2025-01-03 21:04:03 +0600

    feat:added add message functionality in message controller and change message route

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -2,10 +2,43 @@ import prisma from "../lib/prisma.js";
 
 //message.controller.js
 export const addMessage = async (req, res) => {
-  try {
-    res.status(200).json(users);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Failed to add message!" });
-  }
-};
+    const tokenUserId = req.userId;
+    const chatId = req.params.chatId;
+    const text = req.body.text;
+  
+    try {
+      const chat = await prisma.chat.findUnique({
+        where: {
+          id: chatId,
+          userIDs: {
+            hasSome: [tokenUserId],
+          },
+        },
+      });
+  
+      if (!chat) return res.status(404).json({ message: "Chat not found!" });
+  
+      const message = await prisma.message.create({
+        data: {
+          text,
+          chatId,
+          userId: tokenUserId,
+        },
+      });
+  
+      await prisma.chat.update({
+        where: {
+          id: chatId,
+        },
+        data: {
+          seenBy: [tokenUserId],
+          lastMessage: text,
+        },
+      });
+  
+      res.status(200).json(message);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to add message!" });
+    }
+  };
