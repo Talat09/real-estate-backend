4c7d89f1350ae1b9c31e177b23f4c7b09d997819
Talat09 <talat181008312@gmail.com>
2025-01-03 21:52:46 +0600

    update getChats function for only that user message seen who message this user

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -12,10 +12,27 @@ export const getChats = async (req, res) => {
         },
       },
     });
+
+    for (const chat of chats) {
+      const receiverId = chat.userIDs.find((id) => id !== tokenUserId);
+
+      const receiver = await prisma.user.findUnique({
+        where: {
+          id: receiverId,
+        },
+        select: {
+          id: true,
+          username: true,
+          avatar: true,
+        },
+      });
+      chat.receiver = receiver;
+    }
+
     res.status(200).json(chats);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Failed to get chats" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get chats!" });
   }
 };
 export const getChat = async (req, res) => {
